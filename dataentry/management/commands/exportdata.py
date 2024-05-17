a25b21884d797d0df9aa41b03dd8d03a080e957d
kudawoo2002 <kudawoo2002@gmail.com>
2024-05-17 18:48:04 +0000

    exporting data from databse to csv

---
+++ dataentry/management/commands/exportdata.py
@@ -0,0 +1,53 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.apps import apps
+import csv
+import datetime
+
+
+class Command(BaseCommand):
+    help = "Export data from table"
+
+    def add_arguments(self, parser):
+        parser.add_argument("model_name", type=str, help="model name")
+
+    def handle(self, *args, **kwargs):
+        model_name = kwargs['model_name'].capitalize()
+
+        model = None
+
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break
+            except LookupError:
+                pass
+        
+        if not model:
+            raise CommandError(f"model '{model_name}' not found in any apps")
+            return
+
+        # Get data from studens table
+        data = model.objects.all()
+
+        # filepath of the csv file
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+        file_path = f'exported_{model_name}_data_{timestamp}.csv'
+        
+        # Write data to the csv file
+
+        with open(file_path, 'w', newline="") as file:
+            writer = csv.writer(file)
+
+            # Write file header
+
+            writer.writerow([field.name for field in model._meta.fields])
+
+            # Write the data
+
+            for dt in data:
+                writer.writerow([getattr(dt, field.name) for field in model._meta.fields])
+
+        
+        self.stdout.write(self.style.SUCCESS("Data exported successfully"))
+

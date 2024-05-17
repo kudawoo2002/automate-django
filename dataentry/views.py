08a0c3ef1d5599bb63e3787ba189161ed0b3ab09
kudawoo2002 <kudawoo2002@gmail.com>
2024-05-17 19:50:29 +0000

    adding html files and css

--- dataentry/views.py
+++ dataentry/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def importdata(request):
+    return render(request, "dataentry/importdata.html")

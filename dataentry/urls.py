d15546ee43a73009de712b8d46870dd0dfb73121
kudawoo2002 <kudawoo2002@gmail.com>
2024-05-22 06:46:41 +0000

    styling of home page

--- dataentry/urls.py
+++ dataentry/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("import_data/", views.import_data, name="import_data"),
     path("export_data/", views.export_data, name="export_data")
 ]
